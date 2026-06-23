54912e1de5615809536f1f5129b19d1c3aa4cf0c
RominaAir <RominaAir@Mango.homenet.telecomitalia.it>
2026-06-23 22:31:39 +0200

    SEPARATO I PATH PER FONTE

--- 03_Scripts/00_config.R
+++ 03_Scripts/00_config.R
@@ -15,28 +15,27 @@ SIM_DRIVE_EMAIL <- "mipa.sistemainformativo@gmail.com"
 
 # Local project paths
 
+# GENERALI
 DRIVE_DIR_SOURCE    <- "01_Dataset/Source"
-DRIVE_DIR_SOURCE_ANAC <-  "01_Dataset/Source/ANAC"
-DRIVE_DIR_SOURCE_ANAC_GIC2023 <-  "01_Dataset/Source/ANAC/GIC 2023"
-
-
 DRIVE_DIR_PROCESSED <- "01_Dataset/Processed"
-DRIVE_DIR_PROCESSED_ANAC <- "01_Dataset/Processed/ANAC"
-DRIVE_DIR_PROCESSED_ANAC_CIG <-  "01_Dataset/Processed/ANAC/GIC"
-
+DRIVE_DIR_LOGS      <- "05_Logs"
+DRIVE_DIR_DOCS      <- "06_Docs"
 DRIVE_DIR_INDICATORS <- "01_Dataset/Indicators"
 DRIVE_DIR_LISTS     <- "01_Dataset/Lists"
 DRIVE_DIR_METADATA  <- "02_Metadata"
 DIR_SOURCE_MET      <- "02_Metadata/Source_met"
 DRIVE_DIR_OUTPUT    <- "04_Output"
 
+# ANAC
+DRIVE_DIR_SOURCE_ANAC <-  "01_Dataset/Source/ANAC"
+DRIVE_DIR_SOURCE_ANAC_GIC2023 <-  "01_Dataset/Source/ANAC/GIC 2023"
+
+DRIVE_DIR_PROCESSED_ANAC <- "01_Dataset/Processed/ANAC"
+DRIVE_DIR_PROCESSED_ANAC_CIG <-  "01_Dataset/Processed/ANAC/GIC"
+
 # Conto annuale
-DRIVE_DIR_LOGS      <- "05_Logs"
 DRIVE_DIR_LOGS_CA   <- "05_Logs/Conto_annuale"
-
-DRIVE_DIR_DOCS      <- "06_Docs"
-DRIVE_DIR_DOCS      <- "06_Docs/Conto_annuale"
-
+DRIVE_DIR_DOCS_CA      <- "06_Docs/Conto_annuale"
 DRIVE_DIR_INDICATORS_CA <- "01_Dataset/Indicators/Conto_annuale"
 DRIVE_DIR_METADATA_CA  <- "02_Metadata/Conto_annuale"
 DIR_SOURCE_MET_CA      <- "02_Metadata/Source_met/Conto_annuale"
